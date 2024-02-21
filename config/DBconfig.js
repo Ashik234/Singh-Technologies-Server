af326ad91c9c4b305dbd11e64b809335ab2b561f
Ashik k <ashikash796@gmail.com>
2024-02-21 20:03:21 +0530

    Optimized Build Folder

--- config/DBconfig.js
+++ config/DBconfig.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 mongoose.connect(process.env.MONGO_URL,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 }).then(()=>{
     console.log("DB Connection Successful");
 }).catch(err =>console.log("Error Connecting To DB"+err))
